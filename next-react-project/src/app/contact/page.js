893d8d0148bde70c55eb7b6088c35e3ed7e347e3
Jose Murga <josermurgav@gmail.com>
2024-11-29 13:21:30 +0100

    third version

---
+++ next-react-project/src/app/contact/page.js
@@ -0,0 +1,10 @@
+export default function Page()
+{
+    return(
+        <main>
+            <div>
+                <h1>Contact</h1>
+            </div>
+        </main>
+    )
+}
---
+++ next-react-project/src/app/mountain/page.js
@@ -0,0 +1,10 @@
+export default function Page()
+{
+    return(
+        <main>
+            <div>
+                <h1>Mountain Details</h1>
+            </div>
+        </main>
+    )
+}
