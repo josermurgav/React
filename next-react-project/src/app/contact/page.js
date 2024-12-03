4dcc876e022c4e16fa71ebd0373a90e1f903c453
Jose Murga <josermurgav@gmail.com>
2024-12-03 18:17:20 +0100

    sixth

--- next-react-project/src/app/contact/page.js
+++ next-react-project/src/app/contact/page.js
@@ -2,6 +2,20 @@ import { Input } from "postcss";
 
 export default function Page()
 {
+    async function submitForm(formData){
+        "use server";
+        const formFields ={
+            email: formData.get("email"),
+            message: formData.get("message")
+        };
+        console.log("formFields", formfields);
+        console.log(
+            "TODO: Send these form field values to a backend"
+        );
+
+        return formFields;
+    }
+
     return(
         <main className="max-w-md mx-auto p-6 bg-white shadow-md rounded-md">
             <div>
@@ -9,7 +23,7 @@ export default function Page()
                     Contact us!
                 </h1>
             </div>
-            <form className="space-y-4">
+            <form className="space-y-4" action={submitForm}>
                 <div>
                     <label htmlFor="email" className="block text-sm font-medium text-gray-700">
                         Email
@@ -19,7 +33,7 @@ export default function Page()
                         type="email"
                         name="email"
                         required
-                        className="focus:ring-2 "
+                        className="border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                     />
                 </div>
                 <div>
@@ -30,11 +44,13 @@ export default function Page()
                         id="message" 
                         required 
                         name="message" 
-                        rows="4">
-
+                        rows="4"
+                        className="border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                        >
+                        
                     </textarea>
                 </div>
-                <button type="submit">
+                <button type="submit" className="text-white bg-blue-600 rounded-md p-3">
                     Send Message
                 </button>
             </form>            
